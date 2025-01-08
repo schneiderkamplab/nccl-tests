aece84930c27cd526d169311ce373be8a9fc83db
peter-sk <petersk@imada.sdu.dk>
2025-01-08 08:26:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 conda create -n nccl-tests
 conda activate nccl-tests
-conda install nvidia::cuda-cudart nvidia::cuda-nvccl conda-forge::nccl conda-forge::openmpi-mpicc
+conda install nvidia::cuda-cudart nvidia::cuda-nvcc conda-forge::nccl conda-forge::openmpi-mpicc
 ENV_PATH=$(conda info --envs | grep '*' | awk '{print $3;}')
 export MPI=1
 export CUDA_HOME=$ENV_PATH
